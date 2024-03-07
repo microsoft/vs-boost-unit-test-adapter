965d55ec501850d50fd03058259ee844f4eee651
David Raygoza <davidraygoza@microsoft.com>
2024-03-07 15:54:20 -0800

    Add vs-boost-unit-test-adapter for full paths in build+compliance pipeline

--- SetVersion.ps1
+++ SetVersion.ps1
@@ -1,13 +1,13 @@
 Param([parameter(Mandatory=$true)] [string] $version)
 
 $files_to_update = @(
-  "Antlr.DOT\Properties\AssemblyInfo.cs",
-  "BoostTestAdapter\Properties\AssemblyInfo.cs",
-  "BoostTestPackage\Properties\AssemblyInfo.cs",
-  "BoostTestPlugin\Properties\AssemblyInfo.cs",
-  "BoostTestPlugin\source.extension.vsixmanifest",
-  "BoostTestShared\Properties\AssemblyInfo.cs",
-  "VisualStudioAdapter\Properties\AssemblyInfo.cs"
+  "vs-boost-unit-test-adapter\Antlr.DOT\Properties\AssemblyInfo.cs",
+  "vs-boost-unit-test-adapter\BoostTestAdapter\Properties\AssemblyInfo.cs",
+  "vs-boost-unit-test-adapter\BoostTestPackage\Properties\AssemblyInfo.cs",
+  "vs-boost-unit-test-adapter\BoostTestPlugin\Properties\AssemblyInfo.cs",
+  "vs-boost-unit-test-adapter\BoostTestPlugin\source.extension.vsixmanifest",
+  "vs-boost-unit-test-adapter\BoostTestShared\Properties\AssemblyInfo.cs",
+  "vs-boost-unit-test-adapter\VisualStudioAdapter\Properties\AssemblyInfo.cs"
 )
 
 $files_to_update | ForEach-Object {
